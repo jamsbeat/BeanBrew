4baa2e703ad4f149b6b827b86e2a0e032e74b76d
jamesbeaton <beatonj12@gmail.com>
2025-02-04 13:34:01 +0000

    random

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,6 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'is_admin' => 'boolean',
         ];
     }
 
